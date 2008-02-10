e9b24e91a45e992b0ed4971f8e20496805c462ac
enriqueplace <enriqueplace@40327b32-9a3f-0410-91b4-e3c56946e75f>
2008-02-10 18:30:06 +0000

    - se crea estructura base con frontend, backend, e install - se agrega config_general.ini y config_system.ini

---
+++ application/frontend/controllers/NoticiasController.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * NoticiasController
+ * 
+ * @author
+ * @version 
+ */
+
+require_once 'Zend/Controller/Action.php';
+
+class NoticiasController extends Zend_Controller_Action {
+	/**
+	 * The default action - show the home page
+	 */
+	public function indexAction() {
+		// TODO Auto-generated NoticiasController::indexAction() default action
+	}
+
+}
--- html/index.php
+++ html/index.php
@@ -1,20 +1,73 @@
 <?php
 /**
- * My new Zend Framework project
- * 
- * @author  
- * @version 
+ * surforce-base - Zend Framework project
+ *
+ * @author Enrique Place
  */
 
-set_include_path('.' . PATH_SEPARATOR . '../library' . PATH_SEPARATOR . './application/default/models/' . PATH_SEPARATOR . get_include_path());
-require_once 'Zend/Controller/Front.php';
+
+
+/**
+ * Definición de directorios
+ */
+set_include_path(
+    '.' .
+    PATH_SEPARATOR . '../library' .
+    PATH_SEPARATOR . './application/default/models/' .
+    PATH_SEPARATOR . get_include_path()
+);
+
+/**
+ * Carga de clases que se usan constantemente
+ */
+
+include "Zend/Loader.php";
+Zend_Loader::loadClass('Zend_Controller_Front');
+Zend_Loader::loadClass('Zend_Config_Ini');
+Zend_Loader::loadClass('Zend_Registry');
+Zend_Loader::loadClass('Zend_Db');
+Zend_Loader::loadClass('Zend_Db_Table');
+Zend_Loader::loadClass('Zend_Auth');
+
+/**
+ * Configuración del sistema que será leída del config.ini
+ */
+$config_database = new Zend_Config_Ini('./../application/config_system.ini', 'database');
+$config_personalizacion = new Zend_Config_Ini('./../application/config_general.ini', 'personalizacion');
+
+// Permite registra de forma pública las instancias de estas variables de
+// configuración
+$registry = Zend_Registry::getInstance();
+
+$registry->set('config_database', $config_database);
+$registry->set('config_personalizacion', $config_personalizacion);
+
+/**
+ * Configuración inicial
+ */
+error_reporting(E_ALL|E_STRICT);
+date_default_timezone_set('America/Montevideo');
 
 /**
  * Setup controller
  */
 $controller = Zend_Controller_Front::getInstance();
 $controller->setControllerDirectory('../application/default/controllers');
-$controller->throwExceptions(false); // should be turned on in development time 
+
+/**
+ * Todos los módulos que se creen dentro de nuestra aplicación deben de tener
+ * una entrada aquí, en el bootstrap
+ */
+$controller->addControllerDirectory('../application/frontend/controllers', 'frontend');
+$controller->addControllerDirectory('../application/backend/controllers', 'backend');
+$controller->addControllerDirectory('../application/install/controllers', 'install');
+
+$controller->throwExceptions(false); // should be turned on in development time
 
 // run!
 $controller->dispatch();
+
+/**
+ * En el bootstrap (index.php) recomiendan por debug que no se cierre con el tag
+ * tradicional de PHP "?>"
+ */
