485e807edd7c9584cb9d93587814d17ee8cbb368
enriqueplace <enriqueplace@40327b32-9a3f-0410-91b4-e3c56946e75f>
2008-02-10 18:30:06 +0000

    - se crea estructura base con frontend, backend, e install - se agrega config_general.ini y config_system.ini

---
+++ trunk/application/backend/controllers/AutenticacionController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * AutenticacionController
+ *  
+ * @author eplace
+ * @version 
+ */
+
+require_once 'Zend/Db/Table/Abstract.php';
+
+class AutenticacionController extends Zend_Db_Table_Abstract {
+	/**
+	 * The default table name 
+	 */
+	protected $_name = '{1}';
+
+	public function login()
+	{
+		
+	}
+}
